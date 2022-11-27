bb3e8df9231c45c274327669b8a0305c3a305c30
KishoreKumar <119151543+KishoreKumar0603@users.noreply.github.com>
2022-11-27 11:45:14 +0530

    None

---
+++ cuttoffcal.py
@@ -0,0 +1,20 @@
+print(" . . . Welcome to Cuttof Calculator. . . ")
+
+maths=int(input("Enter your Maths score : "))
+
+print(maths,"is your maths score")
+
+physics=int(input("Enter your physics score : "))
+
+print(physics,"is your phys score ")
+
+chms=int(input("Enter your chemistry score : "))
+
+print(chms,"is your chemistry score ")
+
+a=physics/2
+b=chms/2
+c=maths
+
+
+print("Your cuttoff is  ",a+b+c)
